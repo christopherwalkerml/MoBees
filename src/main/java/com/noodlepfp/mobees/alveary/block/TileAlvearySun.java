605c3280646436ee9028e9a7829b5621f1ccaae5
ChuijkYahus <94828194+ChuijkYahus@users.noreply.github.com>
2025-06-27 04:26:20 +0800

    Create zh_cn for 1.20.1 (#1)
    * zh_cn lang create
    
    * zh_cn lang fix
    
    * zh_cn lang fix: to better differentiate between Catalyst and Mutagen
    
    * zh_cn lang fix: change the name for Catalyst even more
    
    * zh_cn lang fix: Emerald
    
    * Texture Update (thanks to Spearkiller)
    
    * updated hive textures + models
    
    * updated crafting ingredients
    
    * updated frames
    
    * updated alveary blocks
    
    * Updated big bees
    
    * small change to large bee animation
    
    * Updated bit sprites
    
    * working on flower box logic. added moon lamp. will work on textures and functionality
    
    * mostly done flower box tile logic. added moon lamp textures
    
    * added nocturnal and diurnal time checks to sun and moon lamp.
    
    * added nocturnal and diurnal time checks to sun and moon lamp.
    
    * working on flower box textures
    
    * working on flower box textures
    
    * working on flower box textures
    
    * working on alveary box textures
    
    * zh_cn lang update: Moon Lamp
    
    ---------
    
    Co-authored-by: Spearkiller <bradley56789@gmail.com>
    Co-authored-by: christopherwalkerml <christopherwalkerml@gmail.com>

--- src/main/java/com/noodlepfp/mobees/alveary/block/TileAlvearySun.java
+++ src/main/java/com/noodlepfp/mobees/alveary/block/TileAlvearySun.java
@@ -1,17 +1,22 @@
 package com.noodlepfp.mobees.alveary.block;
 
 import com.noodlepfp.mobees.alveary.MoreBeesBlockAlvearyType;
-import com.noodlepfp.mobees.alveary.MoreBeesTileActivatable;
+import com.noodlepfp.mobees.alveary.MoreBeesTilePowerable;
 import forestry.api.apiculture.IBeeModifier;
+import forestry.api.apiculture.genetics.BeeLifeStage;
 import forestry.api.genetics.IGenome;
+import forestry.api.genetics.IIndividual;
+import forestry.api.genetics.alleles.BeeChromosomes;
+import forestry.api.genetics.capability.IIndividualHandlerItem;
 import forestry.api.multiblock.IAlvearyComponent;
 import forestry.apiculture.multiblock.MultiblockLogicAlveary;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
 
 import static com.noodlepfp.mobees.alveary.MoreBeesBlockAlveary.LIGHT_LEVEL;
+import static forestry.api.genetics.alleles.ForestryAlleles.ACTIVITY_DIURNAL;
 
-public class TileAlvearySun extends MoreBeesTileActivatable implements IAlvearyComponent.BeeModifier<MultiblockLogicAlveary> {
+public class TileAlvearySun extends MoreBeesTilePowerable implements IAlvearyComponent.BeeModifier<MultiblockLogicAlveary> {
 
     private final BlockState state;
     private final BlockPos pos;
@@ -19,7 +24,11 @@ public class TileAlvearySun extends MoreBeesTileActivatable implements IAlvearyC
     private final IBeeModifier MODIFIER = new IBeeModifier() {
         @Override
         public boolean isAlwaysActive(IGenome genome) {
-            return getWorkingTime() > 0;
+            if (isActiveBeeDiurnal()) {
+                // if active bee is diurnal, sun lamp will make bee always active
+                return getWorkingTime() > 0;
+            }
+            return false;
         }
     };
 
@@ -45,6 +54,18 @@ public class TileAlvearySun extends MoreBeesTileActivatable implements IAlvearyC
 
     public int getLightLevel() {
         return getBlockState().getValue(LIGHT_LEVEL);
+    }
+
+    private boolean isActiveBeeDiurnal() {
+        IIndividualHandlerItem handler = IIndividualHandlerItem.get(getBeeInventory().getQueen());
+
+        if (handler != null && handler.getStage() == BeeLifeStage.QUEEN) {
+            IIndividual queen = handler.getIndividual();
+            IGenome genome = queen.getGenome();
+
+            return genome.getActiveAllele(BeeChromosomes.ACTIVITY) == ACTIVITY_DIURNAL;
+        }
+        return false;
     }
 
     @Override
